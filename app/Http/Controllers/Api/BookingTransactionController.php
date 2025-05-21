bd099f0e78d6d9f45e63c0d1fd2c89a464d14a48
Fathurrahman <10190031@bsi.ac.id>
2025-05-21 15:47:02 +0700

    feat: create booking details

--- app/Http/Controllers/Api/BookingTransactionController.php
+++ app/Http/Controllers/Api/BookingTransactionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreBookingTransactionRequest;
 use App\Http\Resources\Api\BookingTransactionResource;
+use App\Http\Resources\Api\ViewBookingResource;
 use App\Models\BookingTransaction;
 use App\Models\OfficeSpace;
 use Illuminate\Http\Request;
@@ -21,12 +22,30 @@ class BookingTransactionController extends Controller
         $validatedData['booking_trx_id'] = BookingTransaction::generateUniqueTrxId();
         $validatedData['duration'] = $officeSpace->duration;
         $validatedData['ended_at'] = (new \DateTime($validatedData['started_at']))
-        ->modify("+{$officeSpace->duration} days")->format('Y-m-d');
+            ->modify("+{$officeSpace->duration} days")->format('Y-m-d');
 
         $bookingTransaction = BookingTransaction::create($validatedData);
 
         // return response hasil transaksi
         $bookingTransaction->load('officeSpace');
         return new BookingTransactionResource($bookingTransaction);
+    }
+
+    public function booking_details(Request $request)
+    {
+        $request->validate([
+            'phone_number' => 'required|string',
+            'booking_trx_id' => 'required|string',
+        ]);
+
+        $booking = BookingTransaction::where('phone_number', $request->phone_number)
+            ->where('booking_trx_id', $request->booking_trx_id)
+            ->with(['officeSpace', 'officeSpace.city'])
+            ->first();
+
+        if (!$booking) {
+            return response()->json(['message' => 'Booking not found'], 404);
+        }
+        return new ViewBookingResource($booking);
     }
 }
